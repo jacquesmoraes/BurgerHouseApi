52d7b644281a0fa9f35b8908750e1fef56c8da05
jacquesmoraes <jacquesbmoraes@hotmail.com>
2023-11-27 00:23:48 -0300

    category filter

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Category } from '../shared/models/category';
 
 @Component({
   selector: 'app-shop',
@@ -9,14 +10,25 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit{
 products : Product[] = [];
+categories: Category[] = [];
 
 constructor(private shopService : ShopService){}
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error)
-    })
+   this.getProducts();
+   this.getCategories();
   }
+getProducts(){
+  this.shopService.getProducts().subscribe({
+    next: response => this.products = response.data,
+    error: error => console.log(error)
+  })
+}
+getCategories(){
+  this.shopService.getCategories().subscribe({
+    next: response => this.categories = response,
+    error: error => console.log(error)
+  })
+}
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
+import { Category } from '../shared/models/category';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class ShopService {
 
   getProducts(){
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getCategories(){
+    return this.http.get<Category[]>(this.baseUrl + 'products/categories');
   }
 }
