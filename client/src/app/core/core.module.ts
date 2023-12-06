8630dea198ee75d2c5821c81375b3f2938b04e22
jacquesmoraes <jacquesbmoraes@hotmail.com>
2023-12-05 23:16:59 -0300

    breadcrumb

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
